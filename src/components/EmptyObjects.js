25ab6b248345d9eec225c02145a6d6ca2603a8ef
Dhary-Anne <ddavilmar2014@fau.edu>
2022-03-17 17:17:27 -0400

    added empty objects

---
+++ src/components/EmptyObjects.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-unused-vars
+const emptyPersonal = {
+    firstName: '',
+    lastName: '',
+    jobTitle:'',
+    phoneNumber: '',
+    email: '',
+    address: '',
+    description: '',
+};
+
+const emptyEducation = {
+    institute: '',
+    location: '',
+    degreeType: '',
+    subject: '',
+    from: '',
+    to: '',
+};
+
+const emptyExperience = {
+    position: '',
+    company: '',
+    location: '',
+    from: '',
+    to: '',
+};
+
+export {emptyPersonal, emptyExperience, emptyEducation};
